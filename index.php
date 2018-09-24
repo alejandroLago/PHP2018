9bb1704bd4b0f46e9373bf7349828edaee4a940e
Administrator <Administrator@pc.localdomain>
2018-09-24 19:50:49 +0200

    cajitas nombre, usuario y fecha

--- index.php
+++ index.php
@@ -7,18 +7,38 @@
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
         
     </head>
-    <body>
+    <body style="background-color: #666">
         <div class="container">
             <div class="row">
+                <div class="col-12">
+                    <h2 class="text-center" style="color:white;">EJEMPLO INICIO SESION EN PHP</h2>
+                </div>
+            </div>
+            <div class="row">
+                
                 <div class="col-4">
-                    <button id="boton1" class="btn btn-primary">Primary</button>
+                    
                     
                 </div>
+                
                 <div class="col-4">
-                    <input id="cajaNombre" name="usuario_nombre" type="text" placeholder="Usuario">
-                    
+                   
+                    <br><br> 
+                    <form action ="index2.php">
+                    <input id="cajaNombre" class="form-control"  type="text" placeholder="Usuario" required="required">
+                    <br>
+                    <input id="cajaPassword" class="form-control"  type="text" placeholder="Contraseña" required="required">
+                    <br>
+                    <button id="boton1" class="btn btn-primary btn-block" type="submit">Primary</button>
+                    <br>
+                     <p>
+                        Year (yyyy-mm-dd):
+                    <input data-validation="date" data-validation-format="yyyy-mm-dd">
+                      </p>
+                    </form> 
                 </div>
                 <div class="col-4"></div>
+                 
             </div>
         </div>
         
@@ -28,19 +48,24 @@
         ?>
     </body>
     <script src="js/jquery-3.3.1.min.js" type="text/javascript"></script>
-    
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
     <script>
         //document ready se ejecuta cuando toda la pagina se ha cargado correctamente
         $(document).ready(function (){
-            $('#cajaNombre').hide();
+           // $('#cajaNombre').hide();
+        });
+        
+        
+        $.validate({
+            lang:'es'
         });
         
         
         
             $('#boton1').click(function(){
                 console.log("llega");
-                $('#cajaNombre').val("why you running?");
-                 $('#cajaNombre').show(2000);
+               // $('#cajaNombre').val("why you running?");
+                // $('#cajaNombre').show(2000);
             });
     
     
